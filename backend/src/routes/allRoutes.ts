030bf5a0e3ad3ebea8e5dd9d65d11796e65af50d
Chill Guy Flow <flowchillguy@gmail.com>
2026-05-07 15:09:09 +0000

    cài package cookie-parser, cấu hình routes cơ bản

---
+++ backend/src/routes/allRoutes.ts
@@ -0,0 +1,26 @@
+import express from "express";
+
+const router = express.Router();
+
+// ============================================================ 
+// PUBLIC ROUTES
+// ============================================================ 
+
+
+// ============================================================ 
+// PRIVATE ROUTES
+// ============================================================ 
+// Cách 1: Gắn middleware bảo vệ trực tiếp vào từng luồng
+// router.use('/users', protectedRoute, userRoute);
+// router.use('/characters', protectedRoute, characterRoute);
+
+/* Cách 2: Nếu bạn có quá nhiều private route, bạn có thể dùng cách này 
+  để bảo vệ tất cả các route khai báo bên dưới nó:
+  
+  router.use(protectedRoute);
+  router.use('/users', userRoute);
+  router.use('/characters', characterRoute);
+  router.use('/inventory', inventoryRoute);
+*/
+
+export default router
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,32 @@
 import express from "express";
+import dotenv from "dotenv";
 import { connectDB } from "./config/database.js";
+import cors from "cors";
+import apiRoutes from "./routes/allRoutes.js";
+import cookieParser from "cookie-parser";
 
 //Cấu hình đọc file env
-import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001;
 
-app.use("/", (req, res) => {
-  res.send("hello world");
-});
+// ============================================================
+// Cài đặt các Middleware toàn cục
+// ============================================================
+
+// Bộ giải mã dữ liệu (Parser)
+// Cho phép Server của bạn đọc được dữ liệu gửi lên dưới định dạng JSON từ phía Frontend (thông qua body của request)
+app.use(express.json());
+
+// Bộ lá chắn bảo mật và cấp phép giao tiếp giữa các nguồn khác nhau
+// Credentials: true: Cho phép trình duyệt gửi kèm các thông tin xác thực như Cookies hoặc headers Authorization khi thực hiện yêu cầu
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+
+// Giúp Server có thể "đọc" và hiểu được các đoạn Cookies mà trình duyệt gửi kèm theo mỗi request
+app.use(cookieParser());
+
+app.use("/", apiRoutes);
 
 connectDB().then(() => {
   app.listen(PORT, () => {
