745d8f8ae39f75f2fbd6398c0920826c6a92ebbe
Chill Guy Flow <flowchillguy@gmail.com>
2026-05-20 15:13:28 +0000

    chore(frontend): scaffold mainLobby UI

--- frontend/src/pages/MainLobbyPage.tsx
+++ frontend/src/pages/MainLobbyPage.tsx
@@ -5,43 +5,9 @@ import { useAuthStore } from "@/stores/useAuthStore";
 import { useThemeStore } from "@/stores/useThemeStore";
 import React from "react";
 import { toast } from "sonner";
-
+import MainLobby from "@/components/lobby/mainLobby.tsx";
 const MainLobbyPage = () => {
-  // Chỉ lấy ra thông tin user
-  const user = useAuthStore((s) => s.user);
-
-  // test
-  const handleOnClickTest = async () => {
-    try {
-      await api.get("/users/test", { withCredentials: true });
-      toast.success("Click test thành công!!!");
-    } catch (error) {
-      toast.error("Click test thất bại!!!");
-      console.error(error);
-    }
-  };
-
-  // theme light/dark
-  const { isDark, toggleTheme } = useThemeStore();
-
-  return (
-    <>
-      <div>
-        {user?.username}
-        <Logout />
-        <Button onClick={handleOnClickTest}>TEST</Button>
-      </div>
-      {/* Nút thử sáng tối */}
-      <div>
-        <Button
-          onClick={toggleTheme}
-          className={isDark ? "bg-slate-800 text-white" : "bg-white text-black"}
-        >
-          {isDark ? "Chế độ Tối" : "Chế độ Sáng"}
-        </Button>
-      </div>
-    </>
-  );
+  return <MainLobby />;
 };
 
 export default MainLobbyPage;
