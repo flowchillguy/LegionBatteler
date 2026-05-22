8c6602fdf4ea669ca938e64483701fd03458d8d9
Chill Guy Flow <flowchillguy@gmail.com>
2026-05-22 06:49:05 +0000

    feat(frontend): complete TopRight layout and integrate theme toggle

--- frontend/src/components/lobby/topRight.tsx
+++ frontend/src/components/lobby/topRight.tsx
@@ -1,5 +1,68 @@
 // Leaderboard Button & Settings
+import { Settings, Trophy, Sun, Moon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useThemeStore } from "@/stores/useThemeStore";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui.figma/dropdown-menu";
 
 export default function TopRight() {
-  return <div>TopRight</div>;
+  const { isDark, toggleTheme } = useThemeStore();
+
+  return (
+    <>
+      <div className="w-full h-full flex gap-2">
+        {/* Leaderboard */}
+        <Button
+          variant="outline"
+          className="flex-1 h-full flex flex-col justify-center gap-1"
+        >
+          <Trophy className="w-6 h-6" />
+          <span className="text-lime-400">Rank #47</span>
+          <span className="text-xs text-slate-400">Leaderboard</span>
+        </Button>
+
+        <div className="h-full flex flex-col">
+          {/* Model dark/light */}
+          <Button 
+          className="h-1/2 aspect-square flex-1 px-3"
+          onClick ={toggleTheme}
+          >
+            {isDark ? (
+              <Sun className="scale-150" />
+            ) : (
+              <Moon className="scale-150" />
+            )}
+          </Button>
+
+          {/* Settings */}
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button
+                variant="outline"
+                className="h-1/2 aspect-square flex-1 px-3"
+              >
+                <Settings className="w-6 h-6 scale-150" />
+              </Button>
+            </DropdownMenuTrigger>
+
+            <DropdownMenuContent className="">
+              <DropdownMenuItem className="cursor-pointer">
+                Logout
+              </DropdownMenuItem>
+              <DropdownMenuItem className="cursor-pointer">
+                Sound
+              </DropdownMenuItem>
+              <DropdownMenuItem className="cursor-pointer">
+                Change Info
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
+    </>
+  );
 }
