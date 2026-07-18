4372f798743910b94b5896b7fbb5674fe0aee58e
Chill Guy Flow <flowchillguy@gmail.com>
2026-07-18 16:03:29 +0000

    feat(backend): update levelUp method in Fortress class

--- backend/src/game/entities/Fortress.ts
+++ backend/src/game/entities/Fortress.ts
@@ -5,29 +5,38 @@ import type {
   TX,
 } from "../types/EntitiesTypes.js";
 
-const DEFAULT_HP = 5000;
+const DEFAULT_HP = 10000;
 const DEFAULT_DEF = 1000;
 const DEFAULT_DEF_FACTOR = 100;
+const HP_HEAL_WHEN_LV_UP = 1000;
 
 export class Fortress implements IDamageable {
   public team: TTeam;
   public position: TPosition = "Fortress";
   public x: TX;
-  private hp: number = DEFAULT_HP;
+  private currentHp: number = DEFAULT_HP;
   private readonly def: number = DEFAULT_DEF;
+  level: number = 0;
 
   constructor(team: TTeam) {
     this.team = team;
     this.x = team === "left" ? 0 : 10;
   }
 
-  public takeDamage(rawDamage: number): void {
+  takeDamage(rawDamage: number): void {
     const defFactor = DEFAULT_DEF_FACTOR / (DEFAULT_DEF_FACTOR + this.def);
     const finalDamage = Math.max(1, rawDamage * defFactor);
-    this.hp -= finalDamage;
+    this.currentHp -= finalDamage;
   }
 
-  public isDead(): boolean {
-    return this.hp <= 0;
+  isDead(): boolean {
+    return this.currentHp <= 0;
+  }
+
+  levelUp() {
+    if (this.level < 5) {
+      this.currentHp += HP_HEAL_WHEN_LV_UP;
+      this.level += 1;
+    }
   }
 }
